4fd9f81e13845e76de58ff40781bf257601471dc
Bartomeu Heer <bheersalva@gmail.com>
2022-11-09 11:58:10 +0100

    routes services

---
+++ src/app/services/route.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Route } from '../interfaces/route.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RouteService {
+
+  private apiURL = 'http://localhost:5432/api/routes/';
+  constructor(private http: HttpClient) { }
+
+   getAll(): Observable<HttpResponse<Route[]>>{
+      return this.http.get<Route[]>(this.apiURL, {observe: 'response'});
+  } 
+  
+  
+  deleteOne(routeID: string):Observable<HttpResponse<Route>>{
+    return this.http.delete<Route>(this.apiURL+'delete/'+ routeID, {observe: 'response'})
+  }
+
+  addOne(route: Route):Observable<HttpResponse<Route>>{
+    return this.http.post<Route>(this.apiURL+'create/', route, {observe: 'response'})
+  }
+}
